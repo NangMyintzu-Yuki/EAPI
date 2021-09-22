1b1015bcc52b4ae31c22581348185ab2c8b35fcd
Nang Myintzu <meemee13318@gmail.com>
2021-09-22 15:09:43 +0630

    Setup Migration Tables

--- database/migrations/2021_09_22_082645_create_reviews_table.php
+++ database/migrations/2021_09_22_082645_create_reviews_table.php
@@ -15,7 +15,12 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
