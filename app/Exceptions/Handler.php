763138d302bcf6e869fcd33e23b992a395fed496
Nang Myintzu <meemee13318@gmail.com>
2021-09-23 14:46:19 +0630

    Handle Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+// use Exception;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,8 +39,22 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // $this->reportable(function (Throwable $e) {
+
+        // });
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+
+            if($request->expectsJson())
+            {
+                if ($request->is('api/*')) {
+                    return response()->json([
+                        'error' => 'Record not found.'
+                    ],Response::HTTP_NOT_FOUND);
+                }
+            }
+
         });
+
     }
+
 }
